32a4ac100d8e82353dc7f71081a3be6835303475
gustavoCOdx <gustavohenriqueb2006@gmai.com>
2022-04-06 16:09:19 -0300

    sla

--- funcoes.js
+++ funcoes.js
@@ -1,28 +1,41 @@
-const fetchPokemon = () =>{
+const fetchPokemon = () => {
 
-const pokemonPromises = []
-let html = ""
+    const pokemonPromises = []
+    let html = ""
 
-for (let i = 1; i <= 150; i++){
-    let url = 'https://pokeapi.co/api/v2/pokemon/' + i
+    for (let i = 1; i <= 150; i++) {
+        let url = 'https://pokeapi.co/api/v2/pokemon/' + i
 
-    pokemonPromises.push(
-        fatch(url)
-        .then(Response => Response.json()  )
-    )
-}
+        pokemonPromises.push(
+            fetch(url)
+                .then(Response => Response.json())
+        )
+    }
 
-Promise.all(pokemonPromises)
-    .then(pokemons => {
-    pokemons.forEach(pokemon => {
-        html +=  `
-        <li class="cartao>
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+            pokemons.forEach(pokemon => {
+                let tipo = console.dir(pokemon.types[0].type.name)
+                let id_imagem
+                if (pokemon.id < 10) {
+                    id_imagem = '00' + pokemon.id
+                } else if (pokemon.id < 100) {
+                    id_imagem = '0' + pokemon.id
+                } else {
+                    id_imagem = pokemon.id
+                }
+                html += `
+        <li class="cartao ${tipo}">
         <h2 class="cartao-titulo">${pokemon.id} - ${pokemon.name}</h2>
-        <img src="${pokemon.sprites.front_default}">
+        <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id_imagem}.png">
+        <h3 class="cartao-subtitulo">${tipo}</h3>
         </li>
         `
-    });
-    document.querySelector(".pokedex").innerHTML = html
-})
+
+            });
+            console.log(html)
+            document.querySelector(".pokedex").innerHTML = html
+
+        })
 }
 fetchPokemon()
